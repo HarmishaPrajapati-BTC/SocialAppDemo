211f56300af758878dbad23da9f8760cb03a6b1c
HarmishaPrajapati-BTC <harmisha.prajapati@botreetechnologies.com>
2019-10-16 18:17:57 +0530

    friend module related changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
   def check_exist_friend_request(user)
     request_sent = Notification.find_by(user_id: user.id, notified_by_id: current_user.id)
   end
+
+  def already_friends(user)
+    current_user.friends.include?(user)
+  end
 end
